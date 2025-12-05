2e987ad208bb805e1c550193d5a0a25ccb41a991
iiinotlll <yegiiiseeya@outlook.com>
2025-12-05 16:24:04 +0800

    feat: add go module download and build in  example_run.sh

--- example_run.sh
+++ example_run.sh
@@ -3,6 +3,12 @@
 rm -r ./exp/
 mkdir ./exp/
 
+set -ex
+
+go mod download
+
+go build ./...
+
 for i in {0..3}; do
   for j in {0..3}; do
     go run cmd/consensusnode/main.go -shard_id="${j}" -node_id="${i}" &
