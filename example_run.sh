23a5db2eb8c4bb5f8704fabc9516ddfa63b27a14
iiinotlll <yegiiiseeya@outlook.com>
2025-12-11 23:36:34 +0800

    style: modify example_run.sh to add variables SHARD_NUM and NODE_NUM

--- example_run.sh
+++ example_run.sh
@@ -1,20 +1,24 @@
 #!/bin/bash
 
-rm -r ./exp/
-mkdir ./exp/
+SHARD_NUM=4
+NODE_NUM=4
+
+rm -rf ./exp/
+mkdir -p ./exp/
 
 set -ex
 
 go mod download
-
 go build ./...
 
-for i in {0..3}; do
-  for j in {0..3}; do
+# Start consensus nodes
+for ((i=0; i<SHARD_NUM; i++)); do
+  for ((j=0; j<NODE_NUM; j++)); do
     go run cmd/consensusnode/main.go -shard_id="${j}" -node_id="${i}" &
   done
 done
 
+# Start the supervisor
 go run cmd/supervisor/main.go -shard_id=0x7fffffff -node_id=0 &
 
 wait
