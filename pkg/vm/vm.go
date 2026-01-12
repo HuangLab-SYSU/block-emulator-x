25a122bd6ce30aa0ecbd5c5b315627c51c2c20e3
iiinotlll <yegiiiseeya@outlook.com>
2026-01-13 00:23:09 +0800

    feat: block vm executor

--- pkg/vm/vm.go
+++ pkg/vm/vm.go
@@ -3,15 +3,12 @@ package vm
 import (
 	"fmt"
 	"math/big"
-	"path/filepath"
 
 	"github.com/ethereum/go-ethereum/common"
-	"github.com/ethereum/go-ethereum/core/rawdb"
 	"github.com/ethereum/go-ethereum/core/state"
 	"github.com/ethereum/go-ethereum/core/state/snapshot"
-	"github.com/ethereum/go-ethereum/core/types"
 	gethvm "github.com/ethereum/go-ethereum/core/vm"
-	"github.com/ethereum/go-ethereum/ethdb/leveldb"
+	"github.com/ethereum/go-ethereum/ethdb"
 	"github.com/ethereum/go-ethereum/params"
 	"github.com/ethereum/go-ethereum/triedb"
 	"github.com/holiman/uint256"
@@ -20,34 +17,28 @@ import (
 )
 
 const (
-	vmDBNamespace       = "vm_trie"
-	defaultLevelCache   = 16
-	defaultLevelHandler = 16
-	vmStateDBPathFmt    = "vm_state_shard_%d_node_%d"
-	snapshotCacheMB     = 1 << 5
-	EIP3855             = 3855
+	snapshotCacheMB = 1 << 5
+	EIP3855         = 3855
 )
 
 type Executor struct {
+	bCtx       gethvm.BlockContext
 	stateDB    *state.StateDB
 	vmChainCfg *params.ChainConfig
 	evmCfg     gethvm.Config
 }
 
-func NewExecutor(cfg config.VMCfg, lp config.LocalParams) (*Executor, error) {
+func NewExecutor(
+	bCtx gethvm.BlockContext,
+	edb ethdb.Database,
+	curRoot [32]byte,
+	cfg config.VMCfg,
+) (*Executor, error) {
 	// Init state db.
-	dbPath := filepath.Join(cfg.VMStateDir, fmt.Sprintf(vmStateDBPathFmt, lp.ShardID, lp.NodeID))
+	trDB := triedb.NewDatabase(edb, &triedb.Config{Preimages: true, IsVerkle: false})
+	dbRoot := curRoot
 
-	level, err := leveldb.New(dbPath, defaultLevelCache, defaultLevelHandler, vmDBNamespace, false)
-	if err != nil {
-		return nil, fmt.Errorf("failed to new a level database: %w", err)
-	}
-
-	db := rawdb.NewDatabase(level)
-	trDB := triedb.NewDatabase(db, &triedb.Config{Preimages: true, IsVerkle: false})
-	dbRoot := types.EmptyRootHash
-
-	sp, err := snapshot.New(snapshot.Config{CacheSize: snapshotCacheMB}, db, trDB, dbRoot)
+	sp, err := snapshot.New(snapshot.Config{CacheSize: snapshotCacheMB}, edb, trDB, dbRoot)
 	if err != nil {
 		return nil, fmt.Errorf("failed to new a snapshot: %w", err)
 	}
@@ -67,6 +58,7 @@ func NewExecutor(cfg config.VMCfg, lp config.LocalParams) (*Executor, error) {
 	}
 
 	return &Executor{
+		bCtx:       bCtx,
 		stateDB:    stateDB,
 		vmChainCfg: &cc,
 		evmCfg:     evmCfg,
@@ -78,14 +70,13 @@ func (e *Executor) StateDB() *state.StateDB {
 }
 
 func (e *Executor) DeployContract(
-	blockCtx gethvm.BlockContext,
 	txCtx gethvm.TxContext,
 	from common.Address,
 	code []byte,
 	value *uint256.Int,
 	gasLimit uint64,
 ) (common.Address, uint64, error) {
-	evm := gethvm.NewEVM(blockCtx, e.stateDB, e.vmChainCfg, e.evmCfg)
+	evm := gethvm.NewEVM(e.bCtx, e.stateDB, e.vmChainCfg, e.evmCfg)
 	evm.SetTxContext(txCtx)
 	// Create(Deploy) a contract.
 	_, contractAddress, gasUsed, err := evm.Create(from, code, gasLimit, value)
@@ -97,15 +88,23 @@ func (e *Executor) DeployContract(
 }
 
 func (e *Executor) CallContract(
-	blockCtx gethvm.BlockContext,
 	txCtx gethvm.TxContext,
 	from, to common.Address,
 	data []byte,
 	value *uint256.Int,
 	gasLimit uint64,
 ) ([]byte, uint64, error) {
-	evm := gethvm.NewEVM(blockCtx, e.stateDB, e.vmChainCfg, e.evmCfg)
+	evm := gethvm.NewEVM(e.bCtx, e.stateDB, e.vmChainCfg, e.evmCfg)
 	evm.SetTxContext(txCtx)
 
 	return evm.Call(from, to, data, gasLimit, value)
+}
+
+func (e *Executor) Commit() ([]byte, error) {
+	root, err := e.stateDB.Commit(e.bCtx.BlockNumber.Uint64(), true, false)
+	if err != nil {
+		return nil, fmt.Errorf("failed to commit state: %w", err)
+	}
+
+	return root[:], nil
 }
